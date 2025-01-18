4ae3e52b74ca605931e0135d8c0287389720c1eb
michal.fingerut <michal.fingerut@infinitylabs.co.il>
2025-01-18 13:17:35 +0200

    configure tests

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: [
+      'src/**/*.{test,spec}.{js,jsx}',
+      'test/**/*.{test,spec}.{js,jsx}',
+    ],
+  },
+});
